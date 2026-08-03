af5d8361a76e5ab34c25432344c94e8acd6478fb
YllkaBublaku <yb70194@>
2026-08-04 00:58:38 +0200

    Initialize Spring Boot project with MySQL Configuration

---
+++ src/main/java/com/dineahead/DineAheadApplication.java
@@ -0,0 +1,13 @@
+package com.dineahead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DineAheadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DineAheadApplication.class, args);
+	}
+
+}
